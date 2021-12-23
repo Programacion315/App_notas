e1764cc262d8e0f02f3c5656823fd7873de7ea95
jluiso315 <jorgeluishuergo88@gmail.com>
2021-12-23 10:55:56 -0500

    Primera version

---
+++ js/app.js
@@ -0,0 +1,66 @@
+document.getElementById('formulario').addEventListener('submit', guardarNota);
+
+
+function guardarNota(e){
+    
+    let titulo = document.getElementById('titulo').value;
+    let descripcion = document.getElementById('descripcion').value;
+
+    const nota = {
+        titulo,
+        descripcion
+    };
+
+    if(localStorage.getItem('notas') === null){
+
+        let notas = [];
+        notas.push(nota);
+        localStorage.setItem('notas', JSON.stringify(notas));
+        
+    }
+    else{
+
+        let notas = JSON.parse(localStorage.getItem('notas'));
+        notas.push(nota);
+        localStorage.setItem('notas', JSON.stringify(notas))
+        
+    }
+       
+    dameNota();
+    e.preventDefault();
+
+
+}
+
+function dameNota(){
+    let notas = JSON.parse(localStorage.getItem('notas'));
+    let vistaNotas = document.getElementById('notas');
+
+    vistaNotas.innerHTML = '';
+
+    for(let i = 0; i < notas.length; i++){
+
+        let titulo = notas[i].titulo;
+        let descripcion = notas[i].descripcion;
+
+
+        vistaNotas.innerHTML += `<div class="card mb-2">
+
+            <div class="card-body">
+                <p>${titulo} - ${descripcion}</p>
+                <a class="btn btn-danger" onClick="eliminarTarea('${titulo}')">
+                    Borrar
+                </a>
+            </div>
+
+        </div>
+        `
+    }
+}
+
+function eliminarTarea(titulo){
+
+    
+}
+
+dameNota();
